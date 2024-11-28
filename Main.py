71fd9c53fb56ed8f2abe7818f3c1d631d58d3e1f
marione375 <huci.mario375@gmail.com>
2024-11-28 10:50:59 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,27 @@
-import random
 import datetime
 from libro import Aggiunta
 from libro import Prestito
 from libro import Restituzione
 from libro import Catalogo
+from libro import CatalogoPrestiti
 ora = datetime.datetime.now()
 ListaLibri = ["Harry Potter e la pietra filosofale di JK Rowling", "Harry Potter e la camera dei segreti di JK Rowling","Harry Potter e il prigioniero di azkaban","Harry Potter e il calice di fuoco di JK Rowling","HungryGames la ragazza con l'uccello di fuoco",]
+ListaLibriPrestati = ["","","",]
 programma = 1
 while programma!= "no":
     NomeInput = input("Inserisca il suo nome\n")
     CognomeInput =input("Inserisca il suo cognome\n")
-    selezione = input("salve come posso aiutarla\n 1) aggiunta libro\n 2) prestito libro\n 3) riportato un libro\n 4) ricerca nel catalogo\n")
+    selezione = input("salve come posso aiutarla\n 1) aggiunta libro\n 2) prestito libro\n 3) riportato un libro\n 4) ricerca nel catalogo\n 5) lista libri prestati\n")
     if selezione == "1":
         Aggiunta(ListaLibri)
     elif selezione == "2":
-        Prestito(ListaLibri)
+        Prestito(ListaLibri,ListaLibriPrestati)
     elif selezione == "3":
-        Restituzione(ListaLibri,NomeInput,CognomeInput)
+        Restituzione(ListaLibri,NomeInput,CognomeInput,ListaLibriPrestati)
     elif selezione == "4":
         Catalogo(ListaLibri)
+    elif selezione == "5":
+        CatalogoPrestiti(ListaLibriPrestati)
     else:
         programma = input ("Desideri chiudere il programma?\n [si o no]")
 else:
